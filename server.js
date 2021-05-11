30765b7790f3ff347c11ebc311fcb1396598c269
jatinjha <jatinjha370@gmail.com>
2021-05-12 01:46:01 +0530

    intialize app

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=> res.send('API is working'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>console.log(`app is listening on port ${PORT}`));
